f5a5a1be25c01436fb8dea1d84e05018bf7da60d
SJayasinghe <it19126234@my.sliit.lk>
2021-12-14 02:28:36 +0530

    add some unit testing

--- src/app/user-registration/user-registration.component.spec.ts
+++ src/app/user-registration/user-registration.component.spec.ts
@@ -1,16 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserRegistrationComponent } from './user-registration.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserService } from '../services/user.service';
 
 describe('UserRegistrationComponent', () => {
   let component: UserRegistrationComponent;
   let fixture: ComponentFixture<UserRegistrationComponent>;
+  let userService: UserService;
+  let http: HttpClient;
+  let httpController: HttpClientTestingModule;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserRegistrationComponent ]
+      declarations: [ UserRegistrationComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
     })
     .compileComponents();
+    userService = TestBed.inject(UserService);
+    http = TestBed.inject(HttpClient);
+    httpController = TestBed.inject(HttpClientTestingModule);
+
   });
 
   beforeEach(() => {
@@ -21,5 +32,6 @@ describe('UserRegistrationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(userService).toBeDefined();
   });
 });
